a0cd9accb561eb2e30478f560e9adaee0691bf37
SamuelRuby <youreajockey@gmail.com>
2022-08-10 21:41:38 +0200

    2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,6 @@
--- creates the database hbtn_0d_2 and the user user_0d_2C
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- create the database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+-- create read-only user user_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- grant user_0d_2 read-only permissions
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
