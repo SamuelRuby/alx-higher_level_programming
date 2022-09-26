75dcb0976901a279c11a62bc73b1baba8154bcc3
SamuelRuby <youreajockey@gmail.com>
2022-09-26 22:10:00 +0200

    again 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-""" Fetches basic request with urlib"""
-from urllib import request
+"""script that fetches 'https://alx-intranet.hbtn.io/status' """
+if __name__ == "__main__":
+    import urllib.request
 
-with request.urlopen("https://intranet.hbtn.io/status") as response:
-    r = response.read()
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-          .format(type(r), r, r.decode('utf-8')))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
+            as response:
+        html = response.read()
+    print("Body response:")
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode('UTF8')))
